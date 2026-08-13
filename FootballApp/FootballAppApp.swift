4a127db33830ac3c68527e9e5d03d40a799050c2
cduc <christophe.duc@icloud.com>
2026-08-13 07:56:46 +0200

    Initial Commit

---
+++ FootballApp/FootballAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballAppApp.swift
+//  FootballApp
+//
+//  Created by Christophe Duc on 13.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct FootballAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
